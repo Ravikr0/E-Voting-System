c46c8933f1f148da40eb8f3f7c8f8d37ff84d6f3
Ravi kumar <dmtoravi@gmail.com>
2025-03-29 04:53:56 +0530

    main

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def logout():
 
 # --------------------- MAIN -------------------------
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
